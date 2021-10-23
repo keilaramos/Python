2edd5e45176233572b00acb3bf8b9d8fe903db25
Keila Ramos <65951553+keilaramos@users.noreply.github.com>
2021-10-23 18:04:59 -0300

    Jogo descubra o número
    Pequeno exercício testando python.

---
+++ jogo.py
@@ -0,0 +1,66 @@
+print("Jogos de adivinhação")
+numero_secreto = 14
+chute = input("Digite seu número")
+chute = int(chute)
+print("Você digitou ", chute)
+if(numero_secreto == chute):
+    print("Você acertou")
+else:
+    print("Você errou")
+
+print("Fim do Jogo :)")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
