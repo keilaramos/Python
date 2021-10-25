37439c8121be2cc5f55f47ac7d95d94871019689
Keila Ramos <65951553+keilaramos@users.noreply.github.com>
2021-10-25 18:26:51 -0300

    Exercitando
    Multiplicando inteiro  com string e depois só inteiros.

---
+++ multiplicacao.py
@@ -0,0 +1,9 @@
+numero1 = 10
+numero2 = "20"
+produto = numero1 * numero2
+print("Nesse você tem o número 20 repetido por 10 vezes:", produto, sep=" ")
+
+n3 = 10
+n4 = 20
+mult = n3*n4
+print("Aqui está a multiplicação de 10 por 20:", mult, sep=" ")
