594b591d83364edff106dcb2ba3aba128a4d4847
Keila Ramos <65951553+keilaramos@users.noreply.github.com>
2021-10-25 19:19:48 -0300

    Testando mensagens de acesso

---
+++ usuario.py
@@ -0,0 +1,10 @@
+usuario = input("Informe o usuário do sistema!")
+
+if(usuario == "keila"):
+    print("Seja bem-vinda Keila!")
+elif(usuario == "oseias"):
+    print("Seja bem-vindo Oséias!")
+elif(usuario == "ted"):
+    print("Seja bem-vindo Ted")
+else:
+    print("Usuário não identificado!")
