d127ac83125c90f0335a1f931824813309a1a275
Keila Ramos <65951553+keilaramos@users.noreply.github.com>
2021-10-25 19:51:16 -0300

    mudando nome da variável usada

--- chute.py
+++ chute.py
@@ -1,12 +1,20 @@
+print("*********************************")
+print("Bem vindo ao jogo do chute!")
+print("*********************************")
+
+numero_secreto = 14
 chute = input("Digite seu chute:")
+print("Você digitou " , chute)
 chute = int(chute)
-acertou = chute == 14
-maior = chute > 14
-menor = chute < 14
+
+acertou = chute == numero_secreto
+maior = chute > numero_secreto
+menor = chute < numero_secreto
 
 if (acertou):
     print("Parabéns você acertou!")
 elif(maior):
-    print("Seu chute está errado e é maior que o valor correto!")
+    print("Seu chute é Maior que o número secreto!")
 else:
-    print("Seu chute está errado e é menor que o valor correto!")
+    print("Seu chute é menor que o número secreto!")
+    print("Fim do jogo")
