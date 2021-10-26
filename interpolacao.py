770f21e4edd9385e2b924a5655c152d2d69e1fae
Keila Ramos <65951553+keilaramos@users.noreply.github.com>
2021-10-26 18:50:22 -0300

    Colocando variáveis no meio de textos.

---
+++ interpolacao.py
@@ -0,0 +1,32 @@
+print("*********************************")
+print("Bem vindo ao jogo de Adivinhação!")
+print("*********************************")
+numero_secreto = 14
+total_de_tentativas = 3
+rodada = 1
+
+while (rodada <= total_de_tentativas):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+
+    chute_str = input("Digite o seu número: ")
+    print("Você digitou " , chute_str)
+
+    chute = int(chute_str)
+
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if(acertou):
+        print("Parabéns! Você acertou!")
+        break
+    else:
+        if(maior):
+            print("O seu chute foi maior do que o número secreto!")
+        elif(menor):
+            print("O seu chute foi menor do que o número secreto!")
+
+    rodada = rodada + 1
+
+print("Fim do jogo")
+print("Elaborado pela  Sra. {1} {0}".format("Ramos","Keila"))
